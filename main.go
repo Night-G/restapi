0f0b80c5a91d5fb955d0769329c6d49672a8c2e1
Night-G <61281536+Night-G@users.noreply.github.com>
2020-04-26 09:00:52 +0300

    1

--- main.go
+++ main.go
@@ -10,35 +10,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Book struct (модель)
+//Book Struct
 type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
 	Title  string  `json:"title"`
-	Author *Author `json:"Author"`
+	Author *Author `json:"author"`
 }
 
-//Author struct
+//Author Struct
 type Author struct {
-	FirstName string `json:"firstname"`
+	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 
-//Init Books
+//Init books var as a slice Book struct
 var books []Book
 
-// получение всех книг
-//Response - ответ / Request - запрос
+//Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-// получение одной книги
+//Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	params := mux.Vars(r) // Get params
-	// отбор
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get params
+	//Loop through books and find with id
 	for _, item := range books {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
@@ -48,19 +47,18 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Book{})
 }
 
-// создание новой книги
+//Create a New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(100)) //Mock id - not safe - just for example
+	book.ID = strconv.Itoa(rand.Intn(100)) //Mock ID - not safe - just for example
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
 }
 
-//  обновление книги
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range books {
 		if item.ID == params["id"] {
@@ -76,9 +74,8 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
-// удаление книги     не работает?
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range books {
 		if item.ID == params["id"] {
@@ -93,15 +90,16 @@ func main() {
 	//Init Router
 	router := mux.NewRouter()
 
-	//Mock data - @todo - implement database
-	books = append(books, Book{ID: "1", Isbn: "112233", Title: "FirstBook", Author: &Author{FirstName: "Name0", Lastname: "LastN0"}})
-	books = append(books, Book{ID: "2", Isbn: "332211", Title: "SecondBook", Author: &Author{FirstName: "Name1", Lastname: "LastN1"}})
-	//books = appen(books, Book{ID: "3", Isbn: "223311", Title: "ThirBook", Author: &Author{FirstName: "Name2", Lastname: "LastN2"}})
-	//route handlers /конец
+	//Mock Data - @todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "112233", Title: " 1 Book", Author: &Author{Firstname: "a0", Lastname: "a1"}})
+	books = append(books, Book{ID: "2", Isbn: "123321", Title: " 2 Book", Author: &Author{Firstname: "g0", Lastname: "g1"}})
+
+	//Route Handlers/Endpoints
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
 	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/api/books", createBook).Methods("POST")
 	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
